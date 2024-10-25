eb5e23987c937fbe193ca0d959a8a5b563a09626
Shella0975 <decrepitoshellamae@gmail.com>
2024-10-25 15:28:36 +0800

    Update chart.js

--- chart.js
+++ chart.js
@@ -1,48 +1,58 @@
-// <block:setup:1>
-const labels = Utils.months({count: 7});
+const DATA_COUNT = 13;
+const labels = ['2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023'];
+
 const data = {
-  labels: labels,
-  datasets: [{
-    label: 'My First Dataset',
-    data: [65, 59, 80, 81, 56, 55, 40],
-    backgroundColor: [
-      'rgba(255, 99, 132, 0.2)',
-      'rgba(255, 159, 64, 0.2)',
-      'rgba(255, 205, 86, 0.2)',
-      'rgba(75, 192, 192, 0.2)',
-      'rgba(54, 162, 235, 0.2)',
-      'rgba(153, 102, 255, 0.2)',
-      'rgba(201, 203, 207, 0.2)'
-    ],
-    borderColor: [
-      'rgb(255, 99, 132)',
-      'rgb(255, 159, 64)',
-      'rgb(255, 205, 86)',
-      'rgb(75, 192, 192)',
-      'rgb(54, 162, 235)',
-      'rgb(153, 102, 255)',
-      'rgb(201, 203, 207)'
-    ],
-    borderWidth: 1
-  }]
-};
-// </block:setup>
+    labels: labels,
+    datasets: [
+        {
+            label: '1st Year',
+            data: [10, 30, 39, 20, 25, 34, 67, 89, 55, 12, 44, 78, 91],
+            borderColor: 'rgba(0, 0, 0, 1)',
+            backgroundColor: 'rgba(0, 0, 0, 0.2)',
+        },
+        {
+            label: '2nd Year',
+            data: [5, 18, 42, 76, 29, 50, 88, 14, 37, 63, 81, 46, 70],
+            borderColor: 'rgba(255, 255, 0, 1)',
+            backgroundColor: 'rgba(255, 255, 0, 0.2)',
+        },
+        {
+            label: '3rd Year',
+            data: [5, 12, 84, 3, 77, 15, 91, 8, 64, 22, 85, 41, 59],
+            borderColor: 'rgba(0, 255, 0, 1)',
+            backgroundColor: 'rgba(0, 255, 0, 0.2)',
+        },
+        {
+            label: '4th Year',
+            data: [8, 45, 6, 32, 72, 83, 21, 90, 54, 79, 17, 86, 43],
+            borderColor: 'rgba(0, 0, 255, 1)',
+            backgroundColor: 'rgba(0, 0, 255, 0.2)',
+        },
 
-// <block:config:0>
+
+    ]
+};
+
 const config = {
-  type: 'bar',
-  data: data,
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  },
-};
-// </block:config>
+    type: 'line',
+    data: data,
+    options: {
+        responsive: true,
+        plugins: {
+            title: {
+                display: true,
 
-module.exports = {
-  actions: [],
-  config: config,
+
+            }
+        },
+        scales: {
+            y: {
+                suggestedMin: 30,
+                suggestedMax: 100,
+            }
+        }
+    },
 };
+
+const myChart = new Chart(document.getElementById('myChart'), config
+);
