6df2f723ec04552a1d99e45bd1ff5cfdd367a579
Shella0975 <decrepitoshellamae@gmail.com>
2024-10-25 16:33:08 +0800

    Update chart.js

--- chart.js
+++ chart.js
@@ -5,25 +5,25 @@ const data = {
     labels: labels,
     datasets: [
         {
-            label: '1st Year',
+            label: '1st Generation',
             data: [10, 30, 39, 20, 25, 34, 67, 89, 55, 12, 44, 78, 91],
-            borderColor: 'rgba(0, 0, 0, 1)',
-            backgroundColor: 'rgba(0, 0, 0, 0.2)',
+            borderColor: 'rgba(0, 0, 255, 1)',
+            backgroundColor: 'rgba(238, 130, 238, 0.2)',
         },
         {
-            label: '2nd Year',
+            label: '2nd Generation',
             data: [5, 18, 42, 76, 29, 50, 88, 14, 37, 63, 81, 46, 70],
-            borderColor: 'rgba(255, 255, 0, 1)',
+            borderColor: 'rgba(0, 0, 255, 1)',
             backgroundColor: 'rgba(255, 255, 0, 0.2)',
         },
         {
-            label: '3rd Year',
+            label: '3rd Generation',
             data: [5, 12, 84, 3, 77, 15, 91, 8, 64, 22, 85, 41, 59],
-            borderColor: 'rgba(0, 255, 0, 1)',
+            borderColor: 'rgba(0, 0, 255, 1)',
             backgroundColor: 'rgba(0, 255, 0, 0.2)',
         },
         {
-            label: '4th Year',
+            label: '4th Generation',
             data: [8, 45, 6, 32, 72, 83, 21, 90, 54, 79, 17, 86, 43],
             borderColor: 'rgba(0, 0, 255, 1)',
             backgroundColor: 'rgba(0, 0, 255, 0.2)',
